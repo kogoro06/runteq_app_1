e437fe323da7ea1de44182c874ec8cf134d04351
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2024-06-04 22:44:47 +0900

    init commit

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Myapp
+  class Application < Rails::Application
+  
+    # 中略
+	  
+    # ここから下5行を追記
+    config.generators do |g|
+      g.helper false             # helper ファイルを作成しない
+      g.test_framework false     # test ファイルを作成しない
+      g.skip_routes true         # ルーティングの記述を作成しない
+    end
+	  # ここまで追記
+  end
+end
